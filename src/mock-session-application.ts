384d329a2f5725a9a6f70a0288b1e0e262a56dd4
Daniel Kocsan <daniel.kocsan@tv-insight.com>
2023-03-08 10:33:43 +0100

    TVI-771: Integrate all endpoints

--- src/mock-session-application.ts
+++ src/mock-session-application.ts
@@ -1,5 +1,6 @@
 import express, { static as expressStatic } from "express";
 import path from "path";
+import cors from "cors";
 import { SERVER_PORT } from "./config";
 import { TEMPLATE_VARIABLES } from "./template-variables";
 import { replacePlaceholders } from "./helpers";
@@ -7,6 +8,7 @@ import { replacePlaceholders } from "./helpers";
 const app = express();
 
 app.use(expressStatic(path.join(__dirname, "public")));
+app.use(cors());
 
 app.get("/:channelId/tracking.js", (req, res) => {
   res.setHeader("Content-Type", "application/javascript");
@@ -27,12 +29,30 @@ app.get("/i.html", (req, res) => {
 });
 
 app.get("/ra.js", (req, res) => {
-  res.setHeader("Content-Type", "application/javascript");
+  res.setHeader("Content-Type", "text/javascript");
 
   const template = path.join(__dirname, "../", "tracking-templates", "tracking.js");
   const content = replacePlaceholders(template, TEMPLATE_VARIABLES);
 
   res.send(content);
+});
+
+app.get("/ra_if.js", (req, res) => {
+  res.setHeader("Content-Type", "text/javascript");
+
+  const trackingIframeTemplate = path.join(__dirname, "../", "tracking-templates", "tracking-iframe.js");
+  const trackingIframeContent = replacePlaceholders(trackingIframeTemplate, TEMPLATE_VARIABLES);
+
+  const trackingTemplate = path.join(__dirname, "../", "tracking-templates", "tracking.js");
+  const trackingContent = replacePlaceholders(trackingTemplate, TEMPLATE_VARIABLES);
+
+  res.send(trackingContent + trackingIframeContent);
+});
+
+app.get("/meta", (req, res) => {
+  res.setHeader("Content-Type", "text/javascript");
+
+  res.send("").status(200);
 });
 
 app.get("/:did/:sid/:ts/i.png", (req, res) => {
