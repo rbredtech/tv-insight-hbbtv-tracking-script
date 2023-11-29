7b10f723fc9a5dd1d1c140a57ba24738bba1b4f9
Andreas Jantschnig <andreas.jantschnig@tv-insight.com>
2023-11-29 16:56:02 +0100

    test for session end upload

---
+++ spec/sessionEndUpload.spec.js
@@ -0,0 +1,40 @@
+const trackingScript = require("./helper/trackingScript.js");
+const pageHelper = require("./helper/page");
+
+const { CHANNEL_ID_TEST_A, CHANNEL_ID_TEST_B } = [9999, 9998];
+const cases = [[true, "localhost:3000"]];
+
+let page;
+
+beforeAll(async () => {
+  page = await pageHelper.get();
+}, 20000);
+
+afterAll(async () => {
+  await page.browser().close();
+}, 20000);
+
+describe.each(cases)("Session End Upload - Consent: %s", (consent, host) => {
+  let sid;
+
+  describe("WHEN Tracking is started", () => {
+    beforeAll(async () => {
+      const content = trackingScript(CHANNEL_ID_TEST_A, host, consent);
+      await page.setContent(content);
+      await page.waitForResponse((request) => request.url().includes("i.gif"));
+      sid = await page.evaluate(`(new Promise((resolve)=>{__hbb_tracking_tgt.getSID(resolve)}))`);
+    }, 20000);
+
+    describe("AND the tracking is re-loaded", () => {
+      beforeAll(async () => {
+        await page.reload();
+        await page.setContent(trackingScript(CHANNEL_ID_TEST_B, host, consent));
+      }, 10000);
+
+      it(`should upload previous session's end timestamp`, async () => {
+        const regex = new RegExp(`${sid}/\\d*/e.gif`);
+        await page.waitForResponse((request) => regex.test(request.url()));
+      });
+    });
+  });
+});
