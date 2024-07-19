b1233698969b11f1e444aa262e4dbd124bfa0305
Andreas Jantschnig <andreas.jantschnig@tv-insight.com>
2024-07-19 14:08:11 +0200

    switch from uglifyjs to terser

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,42 @@
 var gulp = require("gulp");
-var uglify = require("gulp-uglify");
+var terser = require("gulp-terser");
 var htmlmin = require("gulp-htmlmin");
+var minifyInline = require("gulp-minify-inline");
+
+var terserOptions = {
+  ecma: 5,
+  ie8: true,
+  compress: {
+    arrows: false,
+    booleans: false,
+    comparisons: false,
+    conditionals: false,
+    evaluate: false,
+    if_return: false,
+    keep_fargs: true,
+    negate_iife: false,
+    typeofs: false,
+  },
+};
 
 function copyTemplates() {
   return gulp.src("./tracking-templates/*").pipe(gulp.dest("./tracking-templates-minified"));
 }
 
 function minifyJsTemplates() {
-  return gulp
-    .src("./tracking-templates-minified/*.js")
-    .pipe(uglify({
-      keep_fargs: true,
-      module: false,
-      compress: {
-        arguments: false,
-        arrows: false,
-        assignments: false,
-        booleans: false,
-        comparisons: false,
-        conditionals: false,
-        evaluate: false,
-        if_return: false,
-        keep_fargs: true,
-        negate_iife: false
-      },
-    }))
-    .pipe(gulp.dest("./tracking-templates-minified"));
+  return gulp.src("./tracking-templates-minified/*.js").pipe(terser(terserOptions)).pipe(gulp.dest("./tracking-templates-minified"));
 }
 
 function minifyHtmlTemplates() {
-  return gulp.src('./tracking-templates-minified/*.html')
-    .pipe(htmlmin({
-      collapseWhitespace: true,
-    }))
-    .pipe(gulp.dest('./tracking-templates-minified'));
+  return gulp
+    .src("./tracking-templates-minified/*.html")
+    .pipe(minifyInline({ js: terserOptions }))
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      }),
+    )
+    .pipe(gulp.dest("./tracking-templates-minified"));
 }
 
 exports.default = gulp.series(copyTemplates, minifyJsTemplates, minifyHtmlTemplates);
