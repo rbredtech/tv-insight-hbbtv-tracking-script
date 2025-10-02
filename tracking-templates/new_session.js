841a5008c2c8cdd3b39ba110435e151c4f2391e7
Andreas Jantschnig <andreas.jantschnig@tv-insight.com>
2025-10-02 10:09:05 +0200

    don't call sendMeta from new.js

--- tracking-templates/new_session.js
+++ tracking-templates/new_session.js
@@ -17,9 +17,6 @@
       g._updateSessEndTimer = setInterval(g._updateSessEndTs, 1000);
     }
     g._log(LOG_EVENT_TYPE.S_STRT, 'sid='+g._sid+',did='+g._did+',cid='+g._cid);
-    if (g._sendMeta) {
-      setTimeout(g._sendMeta, 1);
-    }
   }
   try {
     var cb = g._cb['{{CB}}'];
