c9e7503cfcb604c28a4a133bb9a669c0b54ec2ca
Andreas Jantschnig <andreas.jantschnig@tv-insight.com>
2025-11-25 16:06:48 +0100

    add missing await

--- spec/helper/page.js
+++ spec/helper/page.js
@@ -4,7 +4,7 @@ async function get(userAgent = undefined) {
   const browser = await puppeteer.launch({ dumpio: false, args: ["--disable-gpu", "--no-sandbox"] });
   const page = await browser.newPage();
   if (userAgent) {
-    page.setUserAgent(userAgent);
+    await page.setUserAgent(userAgent);
   }
   page.on("response", (response) => console.log(response.url(), response.status()));
   return page;
